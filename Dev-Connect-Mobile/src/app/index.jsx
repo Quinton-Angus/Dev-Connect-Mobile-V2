74bc7b5a1a049350aa7c4290dfbdbb35fcb9989e
Quinton Angus <quinton@quintondev.com>
2026-09-11 23:53:45 +0100

    v1.1.8
    Added gradient to splash loading screen in index.jsx

--- Dev-Connect-Mobile/src/app/index.jsx
+++ Dev-Connect-Mobile/src/app/index.jsx
@@ -4,6 +4,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import splash_logo from "../assets/splash_screen_logo.png"
 import { useEffect, useRef } from "react";
 import boot from "../scripts/boot.js"
+import { LinearGradient } from "expo-linear-gradient"
 
 export default function index() {
 
@@ -39,15 +40,17 @@ export default function index() {
     })
 
     return (
-        <SafeAreaView style={splashStyles.safeAreaView}>
-            <View style={splashStyles.screen}>
-                <View style={splashStyles.logo_container}>
-                    <Image source={splash_logo} style={splashStyles.splash_icon} />
+        <LinearGradient style={splashStyles.screen} colors={["#313131", "#080808"]}>
+            <SafeAreaView style={splashStyles.safeAreaView}>
+                <View style={splashStyles.screen}>
+                    <View style={splashStyles.logo_container}>
+                        <Image source={splash_logo} style={splashStyles.splash_icon} />
+                    </View>
+                    <Animated.View style={[splashStyles.spinner_container, {transform: [{rotate}]}]}>
+                        <View style={splashStyles.spinner} />
+                    </Animated.View>
                 </View>
-                <Animated.View style={[splashStyles.spinner_container, {transform: [{rotate}]}]}>
-                    <View style={splashStyles.spinner} />
-                </Animated.View>
-            </View>
-        </SafeAreaView>
+            </SafeAreaView>
+        </LinearGradient>
     )
 }
